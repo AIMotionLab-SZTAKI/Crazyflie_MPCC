constraint is violated during multiple shooting, incorrect result of optimization, otherwise working

MPCC_with_maps.py
@@ -11,16 +11,17 @@ nq = 4
 nomegaB = 3
 ntheta = 1
 nvtheta = 0
+omegapos = nr+nV+nq
 thetapos = nr + nV + nq + nomegaB
 nstates = nr + nV + nq + nomegaB + ntheta + nvtheta
 
-dt = 0.01
+dt = 0.02
 f = 1 / dt
 n_pathpoints = round(10 * f)
 steepness = 1
 
 # drone data
-mass = 0.028/1000  # kg
+mass = 0.028 # kg
 gravitational_accel = 9.80665  # m/s^2
 
 J = cs.diag([1.4e-5, 1.4e-5, 2.17e-5])  # kg*m^2
@@ -42,6 +43,17 @@ quaternion = cs.MX.sym('quaternion', 4, 1)
 vector = cs.MX.sym('vector', 3, 1)
 distance = cs.MX.sym('distance', 1, 1)
 
+direction_func = cs.Function('direction_func', [distance], [cs.vertcat(-cs.sin(distance / length_height_factor),
+                                                                           cs.cos(distance / length_height_factor),
+                                                                           steepness) / length_height_factor])
+
+
+def torque_scaling(u, torque_scaling_xx = 1,  torque_scaling_yy = 1, torque_scaling_zz = 1, descaling=False):
+    """Scales or descales the 4 inputs, the thrust stays unscaled"""
+    if descaling:
+        return np.diag([1, torque_scaling_xx, torque_scaling_yy, torque_scaling_zz]) * u
+    else:
+        return np.diag([1, 1/torque_scaling_xx, 1/torque_scaling_yy, 1/torque_scaling_zz]) @ u
 
 def put_into_vector(u, location):
     """"put  1x1 value to the end of a cs.MX(location, N) matrix"""
@@ -50,13 +62,12 @@ def put_into_vector(u, location):
 
 
 def derivative1(x, u):
-
+    """ Get the derivative with using the utils.py quaternion methods"""
     r = x[:nr, :]
     V = x[nr:nr+nV, :]
     q = x[nr+nV:nr+nV+nq, :]
     omegaB = x[nr+nV+nq:nr+nV+nq+nomegaB, :]
     vtheta = x[:-1]
-
 
     quat_arr = cs.MX.sym('quat_arr', 4, N)
     conj = cs.Function('conj', [quat_arr],
@@ -71,12 +82,8 @@ def derivative1(x, u):
 
     omegaBdot = J_inv @ (u[1:4, :] - cs.cross(x[nr+nV+nq:nr+nV+nq+nomegaB ,:], cs.mtimes(J, x[nr+nV+nq:nr+nV+nq+nomegaB, :])))
 
-    direction = cs.vertcat(-cs.sin(x[thetapos, :] / length_height_factor),
-                           cs.cos(x[thetapos, :] / length_height_factor),
-                           np.tile(steepness, N).reshape(1, N)) \
-                / length_height_factor
 
-    thetadot = cs.sum1(x[nr:nr+nV] * direction)
+    thetadot = cs.sum1(x[nr:nr+nV] * direction_func.map(N))
     return cs.vertcat(rdot, Vdot, qdot, omegaBdot, thetadot)
 
 
@@ -90,12 +97,11 @@ def derivative2(x, u):
     :returns:
         cs.MX(14, N): state derivative matrix
         """
-
-
     rdot = x[nr:nr + nV, :]
 
     V_func = cs.Function('V_func', [quaternion, vector], [liecasadi.SO3(quaternion).act(vector)])
-    Vdot = V_func.map(N)(x[nr + nV:nr + nV + nq, :], cs.repmat(cs.DM([[0], [0], [1]]), 1, N)) @ cs.diag(u[0, :] / mass) \
+
+    Vdot = (V_func.map(N)(x[nr + nV:nr + nV + nq, :], cs.repmat(cs.DM([[0], [0], [1]]), 1, N)) @ cs.diag(u[0, :] / mass)) \
            - create_vector_from_force(np.ones((1, N)) * gravitational_accel, depth=N)
 
     q_func = cs.Function('q_func', [vector, quaternion], [liecasadi.SO3(quaternion).quaternion_derivative(vector, omega_in_body_fixed=True)])
@@ -104,9 +110,7 @@ def derivative2(x, u):
     omegaBdot = J_inv @ (u[1:4, :] - cs.cross(x[nr + nV + nq:nr + nV + nq + nomegaB, :],
                                               cs.mtimes(J, x[nr + nV + nq:nr + nV + nq + nomegaB, :])))
 
-    direction_func = cs.Function('direction_func', [distance], [cs.vertcat(-cs.sin(distance / length_height_factor),
-                                                                           cs.cos(distance / length_height_factor),
-                                                                           steepness) / length_height_factor])
+
 
     thetadot = cs.sum1(x[nr:nr + nV, :] * direction_func.map(N)(x[thetapos, :]))
 
@@ -124,27 +128,24 @@ def dynamics(x, u):
     k4 = derivative2(x + k3 * dt, u)
 
     x_next = x + (k1 + k2 * 2 + k3 * 2 + k4) * dt/6
-    quat_norm = cs.diag(cs.power(cs.sum1(cs.power(x_next[nr+nV:nr+nV+nq, :], 2)), 0.5))
 
     return x_next
 
 
-def MPCC_sim():
-    N = 10
-    r0 = np.array([1, 0, 0])
-    v0 = np.array([0, gyokketto/2, gyokketto/2])
-    q0 = np.array([-0.3826834324, 0, 0, 0.9238795325])
-    omegaB0 = np.array([0, 0, 0])
-    x0 = cs.vertcat(r0, v0, q0, omegaB0, 0)
+def MPCC_sim(horizon, sim_length, x0):
+    N = horizon
 
+    #X_initial = np.array([[10.81*mass], []])
     opti = cs.Opti()
     X = opti.variable(nstates, N+1)
     U = opti.variable(4, N)
+    U_scaled = torque_scaling(U)
 
     path_direction = cs.MX(3, N+1)
     path_direction[0, :] = -cs.sin(X[thetapos, :] / length_height_factor) / length_height_factor
     path_direction[1, :] = cs.cos(X[thetapos, :]) / length_height_factor
     path_direction[2, :] = steepness / length_height_factor
+    path_direction = direction_func.map(N+1)(X[thetapos, :])
 
     path_position = cs.MX(3, N+1)
     path_position[0, :] = cs.cos(X[thetapos, :] / length_height_factor)
@@ -153,16 +154,18 @@ def MPCC_sim():
 
     # dynamics constraints
     opti.subject_to(X[:, 0] == x0)
-    opti.subject_to(X[:, 1:] == dynamics(X[:, 0:-1], U))
+    opti.subject_to(X[:, 1:] == dynamics(X[:, 0:-1], U_scaled))
+    opti.subject_to(opti.bounded(-20, cs.vec(X[10:13, :]), 20))
 
     # input constraint
     #opti.subject_to(opti.bounded(0, cs.vec(U[0, :])), 3)
-    opti.subject_to(opti.bounded(-1e-2, cs.vec(U), 1e-2))
+    opti.subject_to(opti.bounded(0, cs.vec(U[0, :]), 4))
+    opti.subject_to(opti.bounded(-6, cs.vec(U[1:, :]), 6))
 
     # cost function weights
     qc = cs.DM.ones((1, N))
     ql = cs.DM_eye(N)
-    nu = cs.DM.ones(N)
+    nu = 0.001 * cs.DM.ones(N)
 
     # error vector
     err = X[0:nr, 1:] - path_position[:, 1:]
@@ -177,41 +180,52 @@ def MPCC_sim():
     vtheta = cs.sum1(X[nr:nr+nV, 1:] * path_direction[:, 1:])
 
     # cost function
-    opti.minimize(err_lag @ ql @ err_lag.T + cs.dot(cs.sum1(err_con ** 2), qc) - vtheta @ nu)
+    opti.minimize(err_lag @ ql @ err_lag.T + cs.dot(cs.sum1(err_con ** 2), qc) )
 
     ulist = np.array([[], [], [], []])
     poslist = np.array([[], [], []])
-    for i in range(50):
+    for i in range(sim_length):
 
         # calling the solver
-        p_opts = {"expand": False}
-        s_opts = {"max_iter": 100}
+        p_opts = {"expand": True}
+        s_opts = {"max_iter": 3000}
         opti.solver("ipopt", p_opts, s_opts)
         sol = opti.solve()
         # print('x:', np.array2string(sol.value(X[:, :]), separator=", "))
         # print('u:', np.array2string(sol.value(U[:, :]), separator=", "))
         N = 1
-        x0 = dynamics(x0, sol.value(U)[:, 0, None])
-        N = 10
+        print('x0:', x0)
+        print('x0 in opti:', sol.value(X[:, 0]))
+        x0 = dynamics(x0, torque_scaling(sol.value(U)[:, 0, None]))
+        x0[nr+nV:nr+nV+nq] = Quaternion(x0[nr+nV:nr+nV+nq]).normalized().wxyz
+        print('x1 given by the dynamics', x0)
+        print('x1 in sol.value(X):', sol.value(X[:, 1]))
+        N = horizon
+
         # set initial X guess
         X_initial = np.array(sol.value(X[:, 2:]))
         X_initial = np.append(X_initial, X_initial[:, -1, None], axis=1)
+        X_initial[nr+nV:nr+nV+nq] = Quaternion(X_initial[nr+nV:nr+nV+nq]).normalized().wxyz
         opti.set_initial(X[:, 1:], cs.DM(X_initial))
+
+        if i == 100:
+            pass
 
         # set initial U guess
         U_initial = np.array(sol.value(U[:, 1:]))
         U_initial = np.append(U_initial, U_initial[:, -1, None], axis=1)
         opti.set_initial(U, U_initial)
-        ulist = np.append(ulist, U_initial[:, 0, None], axis=1)
+        ulist = np.append(ulist, torque_scaling(sol.value(U[:, :]))[:, 0, None], axis=1)
         poslist = np.append(poslist, x0[0:3, 0], axis=1)
         X[:, 0] = x0
-
+        print(x0)
+        print(sol.value(U)[:, 0, None])
         # print("X_initial:", X_initial)
         # print("U_initial:", U_initial)
     return ulist, poslist
 
     """one line rk4 step:
-    k1 = derivative(x, u, path_direction)
+    k1 = derivative(x, u3, path_direction)
     k2 = derivative(x + (derivative(x, u, path_direction) * 0.5), u, path_direction)
     k3 = derivative(x + (derivative(x + (derivative(x, u, path_direction) * 0.5), u, path_direction) * 0.5), u, path_direction)
     k4 = derivative(x + derivative(x + (derivative(x + (derivative(x, u, path_direction) * 0.5), u, path_direction) * 0.5), u, path_direction), u, path_direction))

scratch.py
@@ -1,31 +1,60 @@
 import numpy as np
-import quaternion
 import casadi as cs
 import liecasadi as liecs
+from MPCC_with_maps import dynamics
 
 
 
-def qqmult(q1, q2):
-    """multiply two quaternions or a quaternion and a vector"""
-    M = q1.shape[1]
-    a = liecs.SO3(q1[:, 0])
-    b = liecs.SO3(q2[:, 0])
-    q3 = (a * b).xyzw
-    for i in range(1, M):
-        a = liecs.SO3(q1[:, i])
-        b = liecs.SO3(q2[:, i])
-        q3 = cs.horzcat(q3, (a * b).xyzw)
-    return q3
 J = cs.DM(np.array([[3, 0, 0], [0, 2, 0], [0, 0, 1]]))
 a = cs.MX.sym('a', 3)
 v1 = cs.DM(np.array([[1, 10, 1, 10], [2, 20, 2, 20], [3, 30, 3, 30]])).T
 v2 = cs.DM(np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]]))
 v3 = cs.DM(np.array([1, 2, 3, 4]))
-
+gyokketto = np.sqrt(2)
 q = cs.MX.sym('q', 4, 1)
 v = cs.MX.sym('v', 3, 1)
 
 f = cs.Function('f', [q, v], [liecs.SO3(q).act(v)])
 g = f.map(3)
-print(g(v1, v2))
+
+J = cs.diag([1.4e-5, 1.4e-5, 2.17e-5])
+J_inv = cs.diag([1/1.4e-5, 1/1.4e-5, 1/2.17e-5])
+u = cs.DM([[1], [-0.24378298/200], [0.04773571/200], [0.59525322/200]])
+omega = cs.DM([[0], [0], [0]])
+def der(tau, omega):
+    omegaBdot = J_inv @ (tau[1:] - cs.cross(omega, cs.mtimes(J, omega)))
+    return omegaBdot
+
+r0 = np.array([1, 0, 0])
+v0 = np.array([0, gyokketto/2, gyokketto/2])
+q0 = np.array([-0.3826834324, 0, 0, 0.9238795325])
+omegaB0 = np.array([0, 0, 0])
+x0 = cs.DM(cs.vertcat(r0, v0, q0, omegaB0, 0))
+
+u = np.array([[5.0207591611609175e-06], [-0.8065217107973697], [-1.3450114589325268], [2.169850499315401]])
+x0 = np.array([[0.9999999151136898], [0.042426512380644925], [0.024774581429671316],[-5.126342439930953e-06], [0.7071100943676513], [0.11871198934274663], [-0.20975236746069792], [-0.7959460305881096], [0.1125444753709605], [0.5566036307814921], [-0.10507416368934219], [-59.98494593591962], [20.062102163283132], [0.047511926160426814]])
+
+control = dynamics(x0, u)
+0.9999999988171033
+0.014142161423363634
+0.012180830463263638
+-2.415311683713044e-07
+0.7071093858427471
+0.5109762193256931
+-0.42804520011692027
+-0.052035305745572136
+0.1295033372818031
+0.8928875460464717
+-10.080058483045118
+-19.99992531945695
+19.998622113506
+0.01861286621777216
+
+k1 = der(u, omega)
+k2 = der(u, omega + k1 * 0.02 / 2)
+k3 = der(u, omega + k2 * 0.02 / 2)
+k4 = der(u, omega + k3 * 0.02)
+print(k1, k2, k3, k4)
+print((k1 + k2*2 + k3 * 2 + k4) * 0.02 / 6)
+print(control)
 #print(np.cross([10, 20, 30], [11, 21, 31]))